0033261fbb9972cce782c5dabaf6150eb37cbeda
Yusuf Ogundiji <ogundijiyusuf@gmail.com>
2021-07-02 09:29:48 +0100

    add single camp cqrs

--- CodeCamp.Conference.Application/Features/Camps/Query/GetSingleCamp/CampDto.cs
+++ CodeCamp.Conference.Application/Features/Camps/Query/GetSingleCamp/CampDto.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using CodeCamp.Conference.Domain.Entities;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeCamp.Conference.Application.Features.Camps.Query.GetSingleCamp
 {
-    class CampDto
+    public class CampDto
     {
+        public Guid CampId { get; set; }
+        public string Name { get; set; }
+        public string Moniker { get; set; }
+        public DateTime EventDate { get; set; }
+        public int Length { get; set; }
+        public string Venue { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public ICollection<Talk> Talks { get; set; }
     }
 }
